fb171b77d4846f18613b2c30870a1b48397a74ce
RFujinami <reonafuji@gmail.com>
2023-05-06 23:04:16 +0900

    アプリのアイコンを変更

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,42 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      list_data: [
+        {
+          data: 'homework'
+        },
+        {
+          data: 'test'
+        }
+      ]
+    }
   },
   methods: {}
 }
 </script>
 
 <template>
-  <h1 align="left">My To Do App</h1><br/>
+  <h2 id="title">ToDoアプリ</h2>
+  <br/><br/>
   <input type="text"/>
   <button>追加</button>
-  <button>完了済みを削除する</button>
-  <ul>
-    <li>
-      <input type="checkbox" checked/>
-      <span class="todo-done">Vueをマスターする</span>
-    </li>
-    <li>
-      <input type="checkbox"/>
-      <span>牛乳を買う</span>
-    </li>
-    <li>
-      <input type="checkbox"/>
-      <span>家賃を払う</span>
-    </li>
-  </ul>
+  <button>削除</button>
+  <br/><br/>
+  <p>リスト項目</p>
+  <div v-for="item in list_data">
+    <input type="checkbox"/>
+    <span>テスト</span>
+    <p>{{ data }}</p>
+  </div>
+
 </template>
 
 <style>
+#title {
+  top: 20px;
+}
+
 body {
   background-color: #eee;
 }
